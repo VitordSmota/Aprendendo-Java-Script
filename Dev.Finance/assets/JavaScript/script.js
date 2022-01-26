cb823504708e1408851579dcc29a4816963a64d1
Vitor Mota <vitormota05@gmail.com>
2022-01-26 16:26:26 -0300

    main

--- Dev.Finance/assets/JavaScript/script.js
+++ Dev.Finance/assets/JavaScript/script.js
@@ -2,15 +2,18 @@
 const Modal = {
   
   open() {
+    //abrindo modal adicionando a classe "active"
     document.querySelector('.modal-overlay').classList.add('active');
   
   },
   close() {
+    //fechando o modal removendo a classe 'active'
     document.querySelector('.modal-overlay').classList.remove('active');
 
-  }, 
+  }
 }
-const transaction = [
+const transactions = [
+  
   {
     id: 1,
     description: 'Luz',
@@ -26,41 +29,53 @@ const transaction = [
   {
     id: 3,
     description: 'Internet',
-    amount: 20000,
+    amount: -20000,
     date: '21/01/2022',
   },
-];
- 
-
-//Eu Preciso somar as entradas
-// depois eu preciso somar as saídas
-//Eu preciso do Total, Removendo das entradas o valor das sáidas
-//
+  {
+    id: 4,
+    description: 'App',
+    amount: 210000,
+    date: '21/01/2022',
+  },
+]
 const Transaction = {
-  incomes() { //Somar as entradas    
-
+  incomes() {
+      //Somar as entradas
   },
-  expense() { //Somar as sáidas
-
+  expenses() { 
+      //Somar as sáidas
   },
-  total() { // Entradas - Saídas
-
-  },
-};
+  total() { 
+      // Entradas - Saídas
+  }
+}
 //Substituir od dados do HTML com os dados do JS
 const DOM = {
-  addTransaction(transaction, index){
-      console.log(transaction)
+  transactionsContainer: document.querySelector('#data-table tbody'),
+  addTransaction(transaction, index) {
     const tr = document.createElement('tr');
-    tr.innerHTML = DOM.innerHTMLTransaction();
+    tr.innerHTML = DOM.innerHTMLTransaction(transaction);
+    DOM.transactionsContainer.appendChild(tr)
   },
-  innerHTMLTransaction() {
-    const html = `<td class="description">Luz</td>
-                        <td class="expense">- R$ 500,00</td>
-                        <td class="date">18/01/2022</td>
+  innerHTMLTransaction(transaction) {
+    const CSSclass = transaction.amount > 0 ? 'income' : 'expense'
+    const amount = utils.formatCurrency(transaction.amount)
+    const html = `<td class="description">${transaction.description}</td>
+                        <td class="${CSSclass}">${transaction.amount}</td>
+                        <td class="date">${transaction.date}</td>
                         <td><img src="./assets/image/minus.svg" alt="Remover transação">
                   </td>`;
     return html;
   },
 }
-DOM.addTransaction(transaction[1]);
+const utils = {
+  formatCurrency(value) {
+    const signal = Number(value) < 0 ? "-" : ""
+     
+    
+  }
+}
+transactions.forEach(function (transaction) {
+  DOM.addTransaction(transaction)
+})
