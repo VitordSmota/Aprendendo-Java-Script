53bafa6e8a96b5a8bfd2f80b6a6a80d0df93583a
Vitor Mota <vitormota05@gmail.com>
2022-01-27 11:33:47 -0300

    atualizando

--- Dev.Finance/assets/JavaScript/script.js
+++ Dev.Finance/assets/JavaScript/script.js
@@ -1,10 +1,10 @@
 /*Open and Close Button "+Nova Transação" */
 const Modal = {
-  
+
   open() {
     //abrindo modal adicionando a classe "active"
     document.querySelector('.modal-overlay').classList.add('active');
-  
+
   },
   close() {
     //fechando o modal removendo a classe 'active'
@@ -13,7 +13,7 @@ const Modal = {
   }
 }
 const transactions = [
-  
+
   {
     id: 1,
     description: 'Luz',
@@ -41,41 +41,65 @@ const transactions = [
 ]
 const Transaction = {
   incomes() {
-      //Somar as entradas
+    //Somar as entradas
   },
-  expenses() { 
-      //Somar as sáidas
+  expenses() {
+    //Somar as sáidas
   },
-  total() { 
-      // Entradas - Saídas
+  total() {
+    // Entradas - Saídas
   }
 }
 //Substituir od dados do HTML com os dados do JS
 const DOM = {
   transactionsContainer: document.querySelector('#data-table tbody'),
+
   addTransaction(transaction, index) {
     const tr = document.createElement('tr');
     tr.innerHTML = DOM.innerHTMLTransaction(transaction);
     DOM.transactionsContainer.appendChild(tr)
   },
+
   innerHTMLTransaction(transaction) {
     const CSSclass = transaction.amount > 0 ? 'income' : 'expense'
     const amount = utils.formatCurrency(transaction.amount)
     const html = `<td class="description">${transaction.description}</td>
-                        <td class="${CSSclass}">${transaction.amount}</td>
+                        <td class="${CSSclass}">${amount}</td>
                         <td class="date">${transaction.date}</td>
                         <td><img src="./assets/image/minus.svg" alt="Remover transação">
                   </td>`;
     return html;
   },
+  updateBalance() {
+    document
+      .getElementById('incomeDisplay')
+      .innerHTML = "soma das Entradas"
+    document
+      .getElementById('expenseDisplay')
+      .innerHTML = "Soma das Saídas"
+    document
+      .getElementById('totalDisplay')
+      .innerHTML = " Entradas - Saídas"
+  }
 }
+// Formatando os números para R$
 const utils = {
   formatCurrency(value) {
     const signal = Number(value) < 0 ? "-" : ""
-     
-    
+
+    value = String(value).replace(/\D/g, "")
+
+    value = Number(value) / 100
+
+    value = value.toLocaleString("pt-br", {
+      style: "currency",
+      currency: "BRL"
+    })
+    return signal + value
   }
 }
 transactions.forEach(function (transaction) {
   DOM.addTransaction(transaction)
 })
+
+DOM.updateBalance()
