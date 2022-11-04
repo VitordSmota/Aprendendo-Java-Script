139ef0c2277ee2b1a72b18b342f73e74d22a678e
VitordSmota <vitormota05@gmail.com>
2022-11-04 09:12:45 -0300

    att

--- jogoMario/js/script.js
+++ jogoMario/js/script.js
@@ -6,19 +6,32 @@ const jump = () => {
     mario.classList.add('jump');
 
     setTimeout(() => {
-        mario.classList.remove('jump')
-    }, 500)
+        mario.classList.remove('jump');
+    }, 500);
 
 }
 
 const loop = setInterval(() => {
-    const pipePosition = pipe.offsetLeft;
-    const marioPosition = window.getComputerStyle(mario).bottom;
 
-    if (pipePosition <= 120) {
+    console.log('loop')
+    const pipePosition = pipe.offsetLeft;
+    const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
+    
+    console.log(marioPosition)
+
+    if (pipePosition <= 120 && pipePosition > 0  && marioPosition < 80) {
 
         pipe.style.animation = 'none';
         pipe.style.left = `${pipePosition}px`;
+         
+        mario.style.animation = 'none';
+        mario.style.bottom = `${marioPosition}px`;
+        
+        mario.src = './img/game-over.png';
+        mario.style.width = '75px';
+        mario.style.marginLeft = '50px'
+
+        clearInterval(loop)
     }
-}, 10)
+}, 10) 
 document.addEventListener('keydown', jump);
