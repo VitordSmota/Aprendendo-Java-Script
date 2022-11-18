536542e3d979206f07c06b8489b08cf1200c2db0
VitordSmota <vitormota05@gmail.com>
2022-11-18 14:41:36 -0300

    att

---
+++ Curso/array/concat.js
@@ -0,0 +1,5 @@
+const filhas = ['Nami', 'Robin']
+const filhos = ['Chopper', 'Sanji']
+const todos = filhas.concat(filhos, 'Zoro', 'Brook')
+
+console.log([].concat([1, 2], [4, 5], [8, 9], 10, [[6,7]]))
--- Curso/array/reduce2.js
+++ Curso/array/reduce2.js
@@ -2,12 +2,14 @@ const alunos = [
   { nome: "João", nota: 7.3, bolsista: true },
   { nome: "Maria", nota: 9.2, bolsista: true },
   { nome: "Pedro", nota: 9.8, bolsista: true },
-  { nome: "Ana", nota: 8.7, bolsista: true },
+  { nome: "Ana", nota: 8.7, bolsista: false },
 ];
-// Desafio 1: Todos os alunos são bolsistas?
-const todosBolsistas = (resultado, bolsista) => resultado && bolsista
-console.log(alunos.map(a => a.bolsista).reduce(todosBolsistas))
+// Desafio 1: todos os alunos são bolsistas? 
+
+const todosBolsistas = obj => obj.bolsista
+const returnboolean = (acc, att)=> acc && att 
 
 // Desafio 2: Algum aluno é bolsista?
-const algumBolsista = (resultado, bolsista) => resultado || bolsista 
-console.log(alunos.map(a => a.bolsista).reduce(algumBolsista))
+console.log(alunos.map(todosBolsistas).reduce(returnboolean))
+
+console.log(alunos.map(todosBolsistas))
